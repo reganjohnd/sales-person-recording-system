dd195e1cf0de78fff6a89ddb68e0fbba0999b51e
reganjohnd <60075511+reganjohnd@users.noreply.github.com>
2020-02-20 11:47:52 +0200

    reading data from salespersons .txt file to match id to name

--- salesperson.cpp
+++ salesperson.cpp
@@ -95,8 +95,55 @@ void salesperson::addSale()
 	cin >> qty;
 
 	sales sales(id, qty, date);
+	 string line;
+	int num_of_lines{};
+
+	ifstream input;
+	input.open("C:/Users/Roger/Documents/salespersons.txt");
+	//input.ignore(1, '\n');
+	while (!input.eof())
+	{
+		getline(input, line);
+		num_of_lines++;
+	}
+	//vector<string> spID, spName;
+	string spID[100], spName[100];
+	//int spID_[100]{};
+
+	for (int i = 0; i < num_of_lines; i++)
+	{
+		getline(input, spID[i], ',');
+		getline(input, spName[i], ' ');
+		input.ignore(1, '\n');
+	}
+	int spID_ = atoi(spID[2].c_str());
+	//for (int i = 0; i < num_of_lines; i++)
+//	{
+//		spID_[i] = atoi(spID[i].c_str());
+//	}
+	cout << "!!!"<<spID_<<"!!!";
+	system("pause");
 
 	salesFigures.open("C:/Users/Roger/Documents/salesfigures.txt", ios::app | ios::out);
+	if (!salesFigures.is_open())
+	{
+		cout << "File Not Found!";
+	}
+	else
+	{
+		salesFigures << date << ",";
+		for (int i = 0; i < num_of_lines; i++)
+		{
+			if(id == spID_)
+			{ 
+				salesFigures << spName[i];
+			}
+		}
+		salesFigures << "," << qty << "," << sales.get_value(qty) << endl;
+		salesFigures.close();
+	}
+
+	/*salesFigures.open("C:/Users/Roger/Documents/salesfigures.txt", ios::app | ios::out);
 	if (!salesFigures.is_open())
 	{
 		cout << "File not found!" << endl;
@@ -113,5 +160,5 @@ void salesperson::addSale()
 		};
 		salesFigures << "," << qty << "," << sales.get_value(qty) << endl;
 		salesFigures.close();
-	}
+	}*/
 }
